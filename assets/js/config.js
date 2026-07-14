0c62af3670c835d258457f562664abb006511752
ran <ransht@gmail.com>
2026-07-14 17:51:35 +0300

    Redesign Pizza Virtuoso website

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,14 +3,19 @@ export const BUSINESS_CONFIG = {
   nameEn: "Pizza Virtuoso",
   mainPhoneDisplay: "03-9504888",
   mainPhoneLink: "tel:039504888",
-  ownerPhoneDisplay: "054-2537257",
-  ownerPhoneLink: "tel:0542537257",
   whatsappNumber: "972542537257",
   whatsappMessage: "שלום, הגעתי דרך האתר של פיצה וירטואוז ורציתי לבצע הזמנה",
-  // Re-enable only when the restaurant wants to show online ordering again.
   externalOrderUrl: "",
   address: "ז׳בוטינסקי 16, מול היכל התרבות, ראשון לציון",
-  kosherText: "כשר רבנות ראשון לציון"
+  kosherText: "כשר רבנות ראשון לציון",
+  openingHoursVerified: false,
+  openingHours: [],
+  deliveryAreasVerified: false,
+  deliveryAreas: [],
+  googleRatingVerified: false,
+  googleRating: null,
+  googleReviewCount: null,
+  googleReviewsUrl: ""
 };
 
 export const WHATSAPP_URL =
